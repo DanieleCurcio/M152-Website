5593febcf447e9eae4c348bf74cbd423d29262ac
Link9001 <89010971+Link9001@users.noreply.github.com>
2023-06-20 20:23:01 +0200

    update images and fix styling. Also add Header logo.

--- src/public/Source/DetailView-Wow.ts
+++ src/public/Source/DetailView-Wow.ts
@@ -1,11 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const videoElement = document.getElementById('wow-detail-View-Video') as HTMLVideoElement;
-    const currentSpeed = document.getElementById('currentSpeed') as HTMLSpanElement;
-    const speedRange = document.getElementById('speedRange') as HTMLInputElement
+document.addEventListener('DOMContentLoaded', InitWowDetailSite);
 
-    speedRange.addEventListener('input', () => {
-        const speed = parseFloat(speedRange.value);
-        videoElement.playbackRate = speed;
-        currentSpeed.textContent = `${speed}x`
-    });
-});
+async function InitWowDetailSite() {
+    // @ts-ignore
+    shaka.polyfill.installAll();
+
+    // @ts-ignore
+    if (shaka.Player.isBrowserSupported()) {
+        await InitABStreaming(location, document.getElementById('wow-detail-View-Video') as HTMLVideoElement, "/public/Assets/Videos/WowDetailView/manifest.mpd");
+    } else {
+        console.error('Browser does not support shaka player!');
+        alert("The site might not work as intended because you browser doesn't support the shaka browser.")
+    }
+}
+
+// @ts-ignore
+async function InitABStreaming(location: Location, videoElement: HTMLVideoElement, pathToManifestFile: string) {
+    let currentURL = new URL(location.href);
+    let existingPath = currentURL.pathname.substring(0, currentURL.pathname.lastIndexOf('/'));
+    currentURL.pathname = `${existingPath}${pathToManifestFile}`
+    currentURL.search = "";
+
+    let player: any;
+    // @ts-ignore
+    window.player = player = new shaka.Player(videoElement);
+    player.addEventListener('error', onErrorEvent);
+
+    try
+    {
+        await player.load(currentURL.toString());
+    }
+    catch (e) {
+        onError(e);
+    }
+
+    function onErrorEvent(event: any) {
+        onError(event.detail);
+    }
+
+    function onError(error: any) {
+        console.error('Error code', error.code, 'object', error);
+    }
+}
--- src/public/Source/index.ts
+++ src/public/Source/index.ts
@@ -1,46 +1,83 @@
-const fadeInTotkImageClass = "totk-fade-in-Image";
-const fadeInWowImageClass = "wow-fade-in-Image";
+document.addEventListener('DOMContentLoaded', InitIndexSite);
 
-const totkSection = document.getElementById('Zelda-Tears-of-the-Kingdom') as HTMLSelectElement;
-const wowSection = document.getElementById('Word-of-Warcraft') as HTMLSelectElement;
+async function InitIndexSite() {
+    CreateIntersectionObserver();
+    // @ts-ignore
+    shaka.polyfill.installAll();
 
-const totkStartIntersectionObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting)
-        {
-            entry.target.classList.add(fadeInTotkImageClass);
-        }
-    });
-}, { threshold: 0.7 });
+    // @ts-ignore
+    if (shaka.Player.isBrowserSupported()) {
+        await InitABStreaming(location, document.getElementById('wow-home-page-video') as HTMLVideoElement, "/public/Assets/Videos/Home/manifest.mpd");
+    } else {
+        console.error('Browser does not support shaka player!');
+        alert("The site might not work as intended because you browser doesn't support the shaka browser.")
+    }
+}
 
-const totkEndIntersectionObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting)
-        {
-            entry.target.classList.remove(fadeInTotkImageClass);
-        }
-    })
-}, { threshold: 0.9 });
+function CreateIntersectionObserver() {
+    const fadeInTotkImageClass = "totk-fade-in-Image";
+    const fadeInWowImageClass = "wow-fade-in-Image";
+    const totkSection = document.getElementById('Zelda-Tears-of-the-Kingdom') as HTMLSelectElement;
+    const wowSection = document.getElementById('Word-of-Warcraft') as HTMLSelectElement;
 
-const wowStartIntersectionObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting)
-        {
-            entry.target.classList.add(fadeInWowImageClass);
-        }
-    });
-}, { threshold: 0.7 });
+    const totkStartIntersectionObserver = CreateStartingIntersectionObserver(fadeInTotkImageClass);
+    const totkEndIntersectionObserver = CreateEndingIntersectionObserver(fadeInTotkImageClass);
+    const wowStartIntersectionObserver = CreateStartingIntersectionObserver(fadeInWowImageClass);
+    const wowEndIntersectionObserver = CreateEndingIntersectionObserver(fadeInWowImageClass);
 
-const wowEndIntersectionObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting)
-        {
-            entry.target.classList.remove(fadeInWowImageClass);
-        }
-    })
-}, { threshold: 0.9 });
+    totkStartIntersectionObserver.observe(totkSection);
+    totkEndIntersectionObserver.observe(totkSection);
+    wowStartIntersectionObserver.observe(wowSection);
+    wowEndIntersectionObserver.observe(wowSection);
 
-totkStartIntersectionObserver.observe(totkSection);
-totkEndIntersectionObserver.observe(totkSection);
-wowStartIntersectionObserver.observe(wowSection);
-wowEndIntersectionObserver.observe(wowSection);
+    function CreateEndingIntersectionObserver(classNameToRemove: string): IntersectionObserver {
+        return new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting)
+                {
+                    entry.target.classList.remove(classNameToRemove)
+                }
+            })
+        }, { threshold: 0.9 })
+    }
+
+    function CreateStartingIntersectionObserver(classNameToAdd: string): IntersectionObserver {
+        return new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting)
+                {
+                    entry.target.classList.add(classNameToAdd)
+                }
+            })
+        }, { threshold: 0.7 })
+    }
+}
+
+// @ts-ignore
+async function InitABStreaming(location: Location, videoElement: HTMLVideoElement, pathToManifestFile: string) {
+    let currentURL = new URL(location.href);
+    let existingPath = currentURL.pathname.substring(0, currentURL.pathname.lastIndexOf('/'));
+    currentURL.pathname = `${existingPath}${pathToManifestFile}`
+    currentURL.search = "";
+
+    let player: any;
+    // @ts-ignore
+    window.player = player = new shaka.Player(videoElement);
+    player.addEventListener('error', onErrorEvent);
+
+    try
+    {
+        await player.load(currentURL.toString());
+    }
+    catch (e) {
+        onError(e);
+    }
+
+    function onErrorEvent(event: any) {
+        onError(event.detail);
+    }
+
+    function onError(error: any) {
+        console.error('Error code', error.code, 'object', error, 'error data', error.data);
+    }
+}
