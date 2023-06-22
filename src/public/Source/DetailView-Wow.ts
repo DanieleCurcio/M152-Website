7fed94f4443f8fcbbfe5e55dad1191231a58d869
Link9001 <89010971+Link9001@users.noreply.github.com>
2023-06-22 15:46:37 +0200

    add audio change and add x values in the source tag

--- src/public/Source/DetailView-Wow.ts
+++ src/public/Source/DetailView-Wow.ts
@@ -42,6 +42,7 @@ async function InitABStreaming(location: Location, videoElement: HTMLVideoElemen
     try
     {
         await player.load(currentURL.toString());
+        enableAudioSelection(player);
     }
     catch (e) {
         onError(e);
@@ -54,4 +55,46 @@ async function InitABStreaming(location: Location, videoElement: HTMLVideoElemen
     function onError(error: any) {
         console.error('Error code', error.code, 'object', error);
     }
+}
+
+function enableAudioSelection(player: any) {
+    let grouping: Map<number, any[]> = player.getVariantTracks().reduce((mapping: Map<number, any[]>, track: any) => {
+        let currentArr = mapping.get(track.audioId);
+        if (currentArr === undefined)
+        {
+            currentArr = [track];
+            mapping.set(track.audioId, currentArr);
+        }
+        else
+        {
+            currentArr.push(track);
+        }
+        return mapping;
+    }, new Map<number, any[]>());
+
+    const audioSelect = document.createElement('select');
+    audioSelect.addEventListener('change', () => {
+        let tracks = grouping.get(parseInt(audioSelect.value)) as any[];
+        console.log(tracks);
+        console.log(player.getStats());
+        let newTrack = tracks.find(value => value.bandwidth === player.getStats().streamBandwidth);
+        console.log(newTrack);
+        player.selectVariantTrack(newTrack, true);
+    });
+
+    player.addEventListener('adaptation', (oldTrack: any, newTrack: any) => {
+        let tracks = grouping.get(parseInt(audioSelect.value)) as any[];
+        let track = tracks.find(t => t.videoBandwidth == newTrack.videoBandwidth);
+        console.log(track);
+        player.selectVariantTrack(track, true);
+    });
+
+    grouping.forEach((_, key) => {
+        const option = document.createElement('option');
+        option.text = key.toString();
+        audioSelect.add(option);
+    });
+
+    const audioContainer = document.getElementById('wow-detail-view-video-controls') as HTMLDivElement;
+    audioContainer.appendChild(audioSelect);
 }
